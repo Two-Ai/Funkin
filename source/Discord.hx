713ba4288a4b5424437d69dd975799c15a0e99f4
TwoAi <twoai.dev@gmail.com>
2021-03-24 00:49:51 -0400

    Only include Discord.hx on desktop builds

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,3 +1,4 @@
+#if desktop
 package;
 
 import Sys.sleep;
@@ -80,3 +81,4 @@ class DiscordClient
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
 }
+#end
